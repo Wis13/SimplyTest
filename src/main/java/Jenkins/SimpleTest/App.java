f85dcc1ac235a36c8234b3cd6818f1db3d01bded
Vadym <vkqa@outlook.com>
2018-11-25 19:41:13 -0800

    for linux

---
+++ src/main/java/Jenkins/SimpleTest/App.java
@@ -0,0 +1,13 @@
+package Jenkins.SimpleTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
