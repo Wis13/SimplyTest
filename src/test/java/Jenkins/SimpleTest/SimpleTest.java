6b333b17766d729be425a75eaa0c6151cf94511e
Vadym Koroliuk <vkqa@outlook.com>
2020-07-28 13:18:53 -0700

    Added Second test

--- src/test/java/Jenkins/SimpleTest/SimpleTest.java
+++ src/test/java/Jenkins/SimpleTest/SimpleTest.java
@@ -3,6 +3,7 @@ package Jenkins.SimpleTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.assertTrue;
@@ -21,10 +22,38 @@ public class SimpleTest {
     }
 
     @Test
-    public void getTitle() throws InterruptedException{
-        driver.navigate().to("https://www.pravda.com.ua/");
+    public void getTitle() throws InterruptedException {
+        driver.get("https://www.pravda.com.ua/");
         Thread.sleep(2000);
         String title = driver.getTitle();
         assertTrue(title.contains("Українська правда"));
+
+    }
+    @Test
+    public void loginForum() throws InterruptedException {
+        driver.get("https://www.pravda.com.ua");
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("body > nav.container_top_nav > ul > li:nth-child(5) > a")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#user-info-nl > a:nth-child(1)")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#frmLogin > div.generic.login_mobile > table > tbody > tr > td:nth-child(1) > input[type=text]:nth-child(3)")).sendKeys("Lt. Colombo");
+        driver.findElement(By.cssSelector("#frmLogin > div.generic.login_mobile > table > tbody > tr > td:nth-child(1) > input[type=password]:nth-child(7)")).sendKeys("pena");
+        driver.findElement(By.cssSelector("#frmLogin > div.generic.login_mobile > table > tbody > tr > td:nth-child(1) > input[type=submit]:nth-child(11)")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#main > tbody > tr:nth-child(2) > td.td_name > h3 > a")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"phorum\"]/div[5]/div[2]/a[1]/span[1]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"body\"]")).sendKeys("хочу что то написать....");
+        Thread.sleep(6000);
+        driver.findElement(By.xpath("//*[@id=\"body\"]")).sendKeys("я передумал...");
+        Thread.sleep(6000);
+        driver.findElement(By.xpath("//*[@id=\"breadcrumb\"]/a[2]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"user-info\"]/span/small/a")).click();
+        Thread.sleep(2000);
+        driver.get("https://www.facebook.com");
+        Thread.sleep(6000);
     }
 }
